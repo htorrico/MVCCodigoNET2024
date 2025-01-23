65cf8bd55201f008eb24d6c5628b1e84d50c1c92
Hugo Torrico <torricomarquezh@gmail.com>
2025-01-22 19:50:52 -0500

    Agregar archivos de proyecto.

---
+++ MVCCodigoNET2024/Controllers/PeopleController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MVCCodigoNET2024.Models;
+
+namespace MVCCodigoNET2024.Controllers
+{
+    public class PeopleController : Controller
+    {
+        public IActionResult Index()
+        {
+
+            List<PersonModel> models = new List<PersonModel>();
+            models.Add(new PersonModel
+            {
+                PersonId = 1,
+                FirstName = "Hugo",
+                LastName = "Torrico"
+            });
+
+            return View(models);
+        }
+    }
+}
---
+++ MVCCodigoNET2024/Models/PersonModel.cs
@@ -0,0 +1,9 @@
+﻿namespace MVCCodigoNET2024.Models
+{
+    public class PersonModel
+    {
+        public int PersonId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+    }
+}
